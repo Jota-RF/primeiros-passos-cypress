b08ded5a3719512028b5abdd861b9005c2579f7a
unknown <jazielfeitoza@gmail.com>
2025-10-27 21:57:24 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -12,7 +12,7 @@ describe('Orange HRM teste', () => {
   }
 
   it('Login-sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
@@ -20,7 +20,7 @@ describe('Orange HRM teste', () => {
     cy.get(selectorsList.dashboardGrid)
   })
   it('Login-fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type('Test')
     cy.get(selectorsList.loginButton).click()
